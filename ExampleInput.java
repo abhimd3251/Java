92ad4abcef083777b2509dca5be63cb563ea84e1
Abhishek Dulange <159032839+abhimd3251@users.noreply.github.com>
2024-02-21 16:50:34 +0530

    Add files via upload

---
+++ ExampleInput.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class ExampleInput{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter number");
+        int a=sc.nextInt();
+        System.out.println("Number : "+a);
+    }
+}
