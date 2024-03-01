810cd7493262ec0ab491162381fff51fe8720164
Abhishek Dulange <159032839+abhimd3251@users.noreply.github.com>
2024-03-01 18:17:40 +0530

    Example Switch

---
+++ ExampleSwitch.java
@@ -0,0 +1,27 @@
+class ExampleSwitch
+{
+    public static void main(String[]args )
+    {
+        int daynum =3;
+
+        switch(daynum)
+        {
+            case 1-> System.out.println("monday");
+            case 2-> System.out.println("tuesday");
+            case 3-> System.out.println("wednesday");
+            case 4-> System.out.println("thursday");
+            case 5-> System.out.println("friday");
+            case 6-> System.out.println("saturday");
+            case 7-> System.out.println("sunday");
+            default-> System.out.println("invalid");
+        }
+    }
+}
+
+
+
+
+
+
+
+
