e641935f011c09634e84b6ece448d3b3afd0d05f
Abhishek Dulange <159032839+abhimd3251@users.noreply.github.com>
2024-03-02 17:10:17 +0530

    Assignment of using Switch

---
+++ BrandsOfCar.java
@@ -0,0 +1,24 @@
+class BrandsOfCar 
+{
+    public static void main(String[]args )
+    {
+        int cars = 8;
+        
+        switch (cars)
+        {
+            case 1-> System.out.println("tata");
+            case 2-> System.out.println("hundyai");
+            case 3-> System.out.println("toyota");
+            case 4-> System.out.println("ford");
+            case 5-> System.out.println("honda");
+            case 6-> System.out.println("audi");
+            case 7-> System.out.println("BMW");
+            case 8-> System.out.println("kia");
+            case 9-> System.out.println("jaguar");
+            case 10-> System.out.println("mahindar");
+        
+        }    
+
+            
+    }
+}
